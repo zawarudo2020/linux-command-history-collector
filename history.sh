0903c8be0fff92742b47c5dad419f22157e144fb
zawarudo2020 <97649653+zawarudo2020@users.noreply.github.com>
2025-05-06 18:22:00 +0800

    Create history.sh

---
+++ history.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+whoami_txt=$(who am i)
+login_ip=$(echo "$whoami_txt" | awk -F"[()]" '{print $2}')
+login_user=$(echo "$whoami_txt" | awk '{print $1}')
+login_tty=$(echo "$whoami_txt" | awk '{print $2}')
+
+> ~/.bash_history
+
+export PROMPT_COMMAND='{
+  cmd=$(history 1 | { read x c; echo "$c"; });
+  pid=$$;
+  pname=$(ps -p $pid -o comm=);
+
+  echo "$(date "+%b %e %T") $(hostname) sudo: '"$login_user"': TTY='"$login_tty"' ; LOGIN_IP='"$login_ip"' ; PWD=$(pwd) ; USER=$(whoami) ; COMMAND=$cmd ; PROCESS=$pname ; PID=$pid ;"
+} >> /var/log/.histlog'
+
+if [ -z "$(readonly -p | grep PROMPT_COMMAND)" ]; then
+  readonly PROMPT_COMMAND
+fi
