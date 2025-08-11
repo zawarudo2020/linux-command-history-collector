c874f35ef2d90fdaf2b5c651e5a59e734c75de6f
zawarudo2020 <97649653+zawarudo2020@users.noreply.github.com>
2025-08-11 13:27:33 +0800

    Update history.sh

--- history.sh
+++ history.sh
@@ -12,7 +12,7 @@ export PROMPT_COMMAND='{
   pid=$$;
   pname=$(ps -p $pid -o comm=);
 
-  echo "$(date "+%b %e %T") $(hostname) USER=$(whoami) ; TTY='"$login_tty"' ; LOGIN_IP='"$login_ip"' ; PWD=$(pwd) ; COMMAND=$cmd ; PROCESS=$pname ; PID=$pid ;"
+  echo "$(date +"%Y-%m-%dT%H:%M:%S%:z") $(hostname) USER=$(whoami) ; TTY='"$login_tty"' ; LOGIN_IP='"$login_ip"' ; PWD=$(pwd) ; COMMAND=$cmd ; PROCESS=$pname ; PID=$pid ;"
 } >> /opt/logs/.histlog'
 
 if [ -z "$(readonly -p | grep PROMPT_COMMAND)" ]; then
