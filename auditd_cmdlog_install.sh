6d7fdb5912a2ff1646ff392220830a7fca83aa37
zawarudo2020 <97649653+zawarudo2020@users.noreply.github.com>
2026-09-22 20:37:26 +0800

    Update auditd_cmdlog_install.sh

--- auditd_cmdlog_install.sh
+++ auditd_cmdlog_install.sh
@@ -5,8 +5,7 @@ systemctl start auditd
 cat >> /etc/audit/rules.d/audit.rules << 'EOF'
 -a always,exit -F arch=b64 -S execve -k cmdlog
 -a always,exit -F arch=b32 -S execve -k cmdlog
--a always,exclude -F gid=wazuh -k cmdlog_exclude
--a always,exclude -F dir=/var/ossec -k cmdlog_exclude
--a always,exclude -F gid=zabbix -k cmdlog_exclude
+-a always,exclude -F gid=wazuh
+-a always,exclude -F gid=zabbix
 EOF
 service auditd restart
